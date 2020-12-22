61b84c3df5e4b18cbb18c3fd07952c26a3324ab2
Ryan <r_snell02@yahoo.com>
2020-12-21 20:20:21 -0500

    Third commit. Added new multiple codes and added file information

--- Delegates.cs
+++ Delegates.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Project: Delegates
+ * Filename: Delegates.cs
+ * Author: R. Snell
+ * Date: 12/21/2020
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +14,13 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
-    class Program
+    class Delegates
     {
         // delegate for a function that receives an int and returns a bool
         public delegate bool NumberPredicate(int number);
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 30, 32, 36, 40, 50, 60, 70, 80, 90, 100 };
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 30, 32, 36, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 1000 };
 
             // create an instance of the NumberPredicate elegate type
             NumberPredicate evenPredicate = IsEven;
@@ -68,6 +76,18 @@ namespace Delegates
 
             // Display the result
             DisplayList("\nUse IsNotMult3 to filter numbers that are not a multiple of 3: ", numbersNot3);
+
+            // Filter numbers that are a multiple of 100
+            List<int> numbersMultiple100 = FilterArray(numbers, IsMultiple100);
+
+            // Display the result
+            DisplayList("\nUse IsMultiple100 to filter numbers that are a multiple of 100: ", numbersMultiple100);
+            
+            // Filter numbers that are not a multiple of 100
+            List<int> numbersNot100 = FilterArray(numbers, IsNotMult100);
+
+            // Display the result
+            DisplayList("\nUse IsNotMult100 to filter numbers that are not a multiple of 100: ", numbersNot100);
         }   // End Main()
 
         // Select an array's element that statiesfies the predicate
@@ -113,6 +133,11 @@ namespace Delegates
         // Determine wheather an int is not a multiple of 3
         private static bool IsNotMult3(int number) => number % 3 == 1;
 
+        // Determine wheather an int is a multiple of 100
+        private static bool IsMultiple100(int number) => number % 100 == 0;
+
+        // Determine wheather an int is not a multiple of 100
+        private static bool IsNotMult100(int number) => number % 100 == 1;
         // Display the elements of a list
         private static void DisplayList(string description, List<int> list)
         {
